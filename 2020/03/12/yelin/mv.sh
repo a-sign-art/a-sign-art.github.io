4a3dec97864455126c3867a953510b07a491dbe0
a-sign-art <a-sign-art@pm.me>
2020-03-13 13:36:49 +0800

    Site updated: 2020-03-13 13:36:46

---
+++ 2020/03/12/yelin/mv.sh
@@ -0,0 +1,12 @@
+
+mv _MG_2068.jpg IMG_2068.jpg
+mv _MG_2070.jpg IMG_2070.jpg
+mv _MG_2076.jpg IMG_2076.jpg
+mv _MG_2095.jpg IMG_2095.jpg
+mv _MG_2105.jpg IMG_2105.jpg
+mv _MG_2109.jpg IMG_2109.jpg
+mv _MG_2127.jpg IMG_2127.jpg
+mv _MG_2143.jpg IMG_2143.jpg
+mv _MG_2150.jpg IMG_2150.jpg
+mv _MG_2161.jpg IMG_2161.jpg
+mv _MG_2177.jpg IMG_2177.jpg
